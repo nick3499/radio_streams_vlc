3bfca94e2220aa0cf2558afd05f0036d662d5485
nick3499 <ntrinen@yahoo.com>
2020-01-14 11:49:18 -0600

    added code
    - added `from os import chdir`
    - added `chdir('/home/foo/scripts/radio_streams_vlc/')` to help find text files when starting locally with a shell script from any directory. wherever the user decides to store `radio_streams_vlc.py`

--- radio_streams_vlc.py
+++ radio_streams_vlc.py
@@ -1,13 +1,14 @@
 #! /snap/bin/pypy3
 '''`radio_streams_vlc` displays a list of Internet radio stations along with
 numbers for users to make selections. Press "h" for help.'''
+from os import chdir
 from csv import reader
 from subprocess import run
 
-
 def station_selection():
-    '''`station_selection()` method displays a list of Internet radio stations 
+    '''`station_selection()` method displays a list of Internet radio stations
 from a CSV database, and prompts the user make a selection.'''
+    chdir('/home/foo/scripts/radio_streams_vlc/')  # helps to find txt files
     cyan = '\x1b[38;2;72;201;176m'  # format text RGB: 72, 201, 176
     yelo = '\x1b[38;2;244;208;63m'
     orng = '\x1b[38;2;220;118;51m'
@@ -27,9 +28,8 @@ from a CSV database, and prompts the user make a selection.'''
     station_num = int(input('Enter station number: '))  # input station number
 
     # pass CLI args to VLC media player, including radion station URL
-    # run(['/snap/bin/vlc', '--intf', 'ncurses', urls[station_num]], check=True)
+    run(['/snap/bin/vlc', '--intf', 'ncurses', urls[station_num]], check=True)
 
 
 if __name__ == '__main__':
     station_selection()
-
