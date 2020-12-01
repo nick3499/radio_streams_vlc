5dbf154a514925f6fc9fe040eec32c69966a723e
nick3499 <ntrinen@yahoo.com>
2020-12-01 10:35:19 -0600

    revised code
    - extensive revision to `with open()` block

--- radio_streams_vlc.py
+++ radio_streams_vlc.py
@@ -1,35 +1,37 @@
-#! /snap/bin/pypy3
-'''`radio_streams_vlc` displays a list of Internet radio stations along with
-numbers for users to make selections. Press "h" for help in ncurses intf.'''
-from os import chdir
+#!/usr/bin/env python
+'''`vlc_radio_wrapper` module contains the `station_selection()` method which
+displays a list of Internet radio stations from which the user chooses a
+station by the number in the left column.'''
 from csv import reader
 from subprocess import run
+
 
 def station_selection():
     '''`station_selection()` method displays a list of Internet radio stations
 from a CSV database, and prompts the user make a selection.'''
-    chdir('/home/foo/scripts/radio_streams_vlc/')  # helps to find txt files
-    cyan = '\x1b[38;2;72;201;176m'  # format text RGB: 72, 201, 176
-    yelo = '\x1b[38;2;244;208;63m'
-    orng = '\x1b[38;2;220;118;51m'
-    reset = '\x1b[0m'  # reset to default
+    c1 = '\x1b[38;2;72;201;176m'  # cyan; format text RGB: 72, 201, 176
+    c2 = '\x1b[38;2;244;208;63m'  # yellow
+    c3 = '\x1b[38;2;220;118;51m'  # orange
+    res = '\x1b[0m'  # reset to default
     urls = {}  # number selection: station URL
+    pwd = '/scripts/py/vlc_radio_wrapper'  # module root directory
 
-    with open('ascii_radio.txt', 'r') as ascii_art:
-        [print(f'{cyan}{line[:-1]}{reset}') for line in ascii_art]  # ASCII art
+    with open(f'{pwd}/txt/ascii_radio.txt', 'r') as ascii_art:
+        [print(f'{c1}{line[:-1]}{res}') for line in ascii_art]  # ASCII art
 
-    with open('stations.csv', 'r') as _file:
-        _reader = reader(_file)  # csv reader object
-        for number, record in enumerate(_reader, 1):
-            print(f'{cyan}{number:>2}{reset}  {yelo}{record[0]:}{reset}  \
-{orng}{record[1]}{reset}')  # print radio station list
-            urls[number] = record[2]  # append number with URL to dictionary
+    # list stations
+    with open(f'{pwd}/csv/stations.csv') as csv_file:
+        csv_rec_list = list(enumerate(reader(csv_file), 1))  # csv reader object
+        for num, rec in csv_rec_list:
+            # print color-formatted radio station list
+            print(f'{c1}{num:>2}{res}  {c2}{rec[0]:}{res}  {c3}{rec[1]}{res}')
+            urls[num] = rec[2]  # append number with URL to dictionary
 
-    station_num = int(input('Enter station number: '))  # input station number
+    station_num = int(input('Enter item number: '))  # input station number
 
-    # pass CLI args to VLC media player, including radion station URL
+    # pass CLI args to VLC media player, including radio station URL
     run(['/snap/bin/vlc', '--intf', 'ncurses', urls[station_num]], check=True)
 
 
 if __name__ == '__main__':
-    station_selection()
+    station_selection()  # if standalone
