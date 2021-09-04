dafd391428ee7e629bcb4a8fc9cbcbcccce7cac1
nick3499 <ntrinen@yahoo.com>
2021-09-04 01:53:01 -0500

    revised script
    - added a callback
    - added a `RADIO STREAMS` logo before the callback

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-#!/usr/bin/env python
-'''Play internet radio selection in VLC media player.
-Shift + M --> meta-info; q --> quit'''
+#!/bin/python3
+'''Play internet radio selection in media player.'''
 from subprocess import run
 from ascii_art import get_ascii_art
 import station_list
 
+# dump ASCII art header
+print(get_ascii_art())
+# dump station list
+station_list.get_station_list()
 
-print(get_ascii_art())  # get color-schemed ASCII art heading
-station_list.get_station_list()  # list stations
 
 def station_selection():
-    '''Play selected internet radio station.'''
-    urls = station_list.urls  # obj: number/station name/genres/url
-    station_num = int(input('Enter item number: '))  # input station number
-    run(['/snap/bin/vlc', '--intf', 'ncurses', urls[station_num]], check=True)  # pass args
+    '''Play selected station.'''
+    # obj: number/station name/genres/url
+    URLS = station_list.URLS
+    # station number selection input
+    station_num = int(input('\x1b[38;2;72;201;176mEnter station number\
+\x1b[0m: '))
+    # ===  VLC  ===
+    # run(['/usr/bin/vlc', '--intf', 'ncurses', URLS[station_num]], check=True)
+    # run(['/usr/bin/vlc', '--audio', '-I', 'qt', URLS[station_num]], check=True)
+    # ===  mpv  ===
+    run(['/usr/bin/mpv', URLS[station_num]], check=True)
+    # ===
+    # color-themed logo
+    c_1 = '\x1b[38;2;220;118;51m'
+    c_2 = '\x1b[38;2;244;208;63m'
+    res = '\x1b[0m'
+    print(f'{c_1}+~+~+~+~+~+~+~+~+~+~+~+~+~+{res}')
+    print(f'{c_1}<{c_2}R{c_1}|{c_2}A{c_1}|{c_2}D{c_1}|{c_2}I{c_1}|{c_2}O{c_1}>\
+{c_2} {c_1}<{c_2}S{c_1}|{c_2}T{c_1}|{c_2}R{c_1}|{c_2}E{c_1}|{c_2}A{c_1}|{c_2}M\
+{c_1}|{c_2}S{c_1}>{res}')
+    print(f'{c_1}+~+~+~+~+~+~+~+~+~+~+~+~+~+{res}')
+    # callback
+    station_selection()
 
 
 if __name__ == '__main__':
