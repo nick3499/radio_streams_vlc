a3d0ed0f89de69137c8fa31526c00d28314384e6
nick3499 <ntrinen@yahoo.com>
2021-09-10 06:25:33 -0500

    revised code
    - revised the input prompt text, including a radio emoji
    - removed _RADIO STREAMS_ ASCII art

--- app.py
+++ app.py
@@ -11,27 +11,24 @@ station_list.get_station_list()
 
 
 def station_selection():
-    '''Play selected station.'''
-    # obj: number/station name/genres/url
+    '''Select and play selected station.'''
+    # hash table of numbers associated with URLs
     URLS = station_list.URLS
-    # station number selection input
-    station_num = int(input('\x1b[38;2;72;201;176mEnter station number\
-\x1b[0m: '))
+    # input station selection number
+    yellow = '\x1b[1;7;38;2;244;208;63m'
+    reset = '\x1b[0m'
+    radio = '\U0001F4FB'
+    station_num = int(input(f'\n{radio} {yellow} Enter station number {reset}: '))
+    print('')
+    # ===  MPV  ===
+    run(['/bin/mpv', '--no-video', URLS[station_num]], check=True)
     # ===  VLC  ===
     # run(['/usr/bin/vlc', '--intf', 'ncurses', URLS[station_num]], check=True)
     # run(['/usr/bin/vlc', '--audio', '-I', 'qt', URLS[station_num]], check=True)
-    # ===  mpv  ===
-    run(['/usr/bin/mpv', URLS[station_num]], check=True)
+    # ===  MPG123  ===
+    # run(['/usr/bin/mpg123', '--cpu', 'x86-64', '-v', '--title', 
+    #      '--long-tag', URLS[station_num]], check=True)
     # ===
-    # color-themed logo
-    c_1 = '\x1b[38;2;220;118;51m'
-    c_2 = '\x1b[38;2;244;208;63m'
-    res = '\x1b[0m'
-    print(f'{c_1}+~+~+~+~+~+~+~+~+~+~+~+~+~+{res}')
-    print(f'{c_1}<{c_2}R{c_1}|{c_2}A{c_1}|{c_2}D{c_1}|{c_2}I{c_1}|{c_2}O{c_1}>\
-{c_2} {c_1}<{c_2}S{c_1}|{c_2}T{c_1}|{c_2}R{c_1}|{c_2}E{c_1}|{c_2}A{c_1}|{c_2}M\
-{c_1}|{c_2}S{c_1}>{res}')
-    print(f'{c_1}+~+~+~+~+~+~+~+~+~+~+~+~+~+{res}')
     # callback
     station_selection()
 
